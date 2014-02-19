65ed3ba4420b5f37e47d80914c43f206f29547c6
Dave Slutzkin <daveslutzkin@fastmail.fm>
2014-02-19 18:03:42 +1100

    Give a user agent to avoid crude server blocking.

--- scraper.php
+++ scraper.php
@@ -23,6 +23,7 @@ print "Scraping wokim.townsville.qld.gov.au...\n";
 // Download and parse RSS feed (last 14 days of applications)
 $curl = curl_init($rss_feed);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; PlanningAlerts/0.1; +http://www.planningalerts.org.au/)");
 $rss_response = curl_exec($curl);
 curl_close($curl);
 
